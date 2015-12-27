e683647b2d7cbc26413defc9a00d59dd340fb0f5
Elana Kopelevich <ekopelevich@gmail.com>
2015-12-27 09:50:17 -0700

    page renders monkeys

--- app.js
+++ app.js
@@ -1,64 +1,27 @@
 $(document).ready(function(){
-  getMonkeyData().then(function( monkeyData ){
-    var monkeyList = buildMonkeyList();
+  getMonkeyData()
+  .then(function( monkeyData ){
+    console.log( monkeyData);
+    var monkeyList = buildMonkeyList( monkeyData );
     renderMonkeyList( monkeyList );
   });
+  attachNewMonkeyHandler();
 });
 
-// http requests
+// http get request
 function getMonkeyData() {
-  return new Promise (function( reject, resolve){
-    $.ajax({
-      method: 'get',
-      url: 'http://localhost:8000/monkeys',
-      success: resolve,
-      error: reject
-    });
+  return $.get('http://localhost:8000/monkeys');
+}
+
+function buildMonkeyList( monkeyData ){
+  return monkeyData.monkeys.map(function( monkey, index ){
+      return convertMonkeyObjectToListItem( monkey );
   });
 }
-
-// attach event handler - return formData??
-function attachNewMonkeyHandler(){
-    $( '.submit-monkey' ).click( function( event ){
-        event.preventDefault();
-
-        var formData = getNewMonkeyData( $(this) );
-        createNewMonkey( formData ).then( function( newMonkey ){
-            addNewMonkeyToList( newMonkey );
-            flashCreationMessage();
-        }).catch(function( error ){
-            console.error( 'Unable to add monkey', error );
-        });
-    });
-}
-
-// get data from form - return data??
-function getNewMonkeyData( form ){
-    var formValues = form.serializeArray();
-    return formValues.reduce(function(formattedMonkey, monkey){
-        formattedMonkey[monkey.name] = monkey.value;
-        return formattedMonkey;
-    }, {});
-}
-
-// create new monkey - return data??
-function createNewMonkey(formData){
-    return new Promise(function(resolve, reject){
-        $.ajax({
-            method: 'post',
-            url: 'http://localhost:8000/monkeys',
-            data: formData,
-            success: resolve,
-            error: reject
-        });
-    });
-}
-
-
 
 function convertMonkeyObjectToListItem( monkey ){
     var monkeyNameContainer = document.createElement('p');
-    var monkeyName = document.createTextNode[monkey.name].join(' ');
+    var monkeyName = document.createTextNode(monkey.name);
     monkeyNameContainer.appendChild(monkeyName);
 
     var monkeyDOBContainer = document.createElement('p');
@@ -77,16 +40,42 @@ function convertMonkeyObjectToListItem( monkey ){
     return monkeyListItem;
 }
 
-function buildMonkeyList( monkeyData ){
-  return monkeyData.monkeys.map(function( monkey, index ){
-      return convertmonkeyObjectToListItem( monkey );
+function renderMonkeyList( monkeyList ){
+  $('.monkey-list').append( monkeyList );
+}
+
+// attach event handler - return formData
+function attachNewMonkeyHandler(){
+    $( '.submit-monkey' ).click( function( event ){
+        event.preventDefault();
+
+        var formData = getNewMonkeyData( $(this) );
+        createNewMonkey( formData )
+        .then( function( newMonkey ){
+            addNewMonkeyToList( newMonkey );
+            flashCreationMessage();
+        })
+        .catch( function( error ){
+            console.error( 'Unable to add monkey', error );
+        });
+    });
+}
+
+// get data from form - return data??
+function getNewMonkeyData( form ){
+    var formValues = form.serializeArray();
+    return formValues.reduce(function(formattedMonkey, monkey){
+        formattedMonkey[monkey.name] = monkey.value;
+        return formattedMonkey;
+    }, {});
+}
+
+// create new monkey - return promise
+function createNewMonkey(formData){
+  return $.post('http://localhost:8000/monkeys', function( data ){
+    console.log('New monkey posted');
   });
 }
-
-function renderMonkeyList( monkeyList ){
-  $('monkey-list').append( monkeyList );
-}
-
 
 
 function addNewMonkeyToList( monkey ){
